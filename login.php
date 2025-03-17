a1f4121e09746d889d1b8c513c7d6b1b19862154
ashan00001 <alakpradeepa@gmail.com>
2025-03-17 13:53:04 +0530

    Update login.php

--- login.php
+++ login.php
@@ -5,3 +5,19 @@ if (isset($_SESSION['username'])) {
     exit();
 }
 $message = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE username = :username');
+    $stmt->execute(['username' => $username]);
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['username'] = $username;
+        header('Location: game.php');
+        exit();
+    } else {
+        $message = '<p class="error">Invalid username or password.</p>';
+    }
+}
