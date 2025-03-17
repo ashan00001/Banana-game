79050a6d49c74da4e904dc7ed6a92d8f147b5c79
ashan00001 <alakpradeepa@gmail.com>
2025-03-17 23:01:21 +0530

    Update game.php

--- game.php
+++ game.php
@@ -125,6 +125,26 @@ $currentHighScore = $result['highscore'] ?? 0;
         window.onload = function() {
             fetchBananaQuestion();
         };
+        document.addEventListener("DOMContentLoaded", function () {
+            const clickSound = new Audio("sounds/click.wav");
+
+            function playClickSound() {
+                const soundClone = clickSound.cloneNode();
+                soundClone.volume = 1.0;
+                soundClone.play().catch(error => console.error("Playback error:", error));
+            }
+
+            document.body.addEventListener("click", function (event) {
+                if (event.target.tagName === "BUTTON") {
+                    playClickSound();
+                }
+            });
+
+            document.body.addEventListener("click", function unlockAudio() {
+                clickSound.play().catch(() => {});
+                document.body.removeEventListener("click", unlockAudio);
+            });
+        });
     </script>
 </body>
 </html>
