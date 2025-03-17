bf3f50b6d4f500e05b4939e919a4e4632988c274
ashan00001 <alakpradeepa@gmail.com>
2025-03-17 13:52:43 +0530

    Update login.php

--- login.php
+++ login.php
@@ -4,47 +4,4 @@ if (isset($_SESSION['username'])) {
     header('Location: game.php');
     exit();
 }
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Banana Puzzle Game</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <h2>Login</h2>
-        <form action="login.php" method="POST">
-            <label for="username">Username:</label>
-            <input type="text" name="username" required><br><br>
-            <label for="password">Password:</label>
-            <input type="password" name="password" required><br><br>
-            <button type="submit" name="login">Login</button>
-        </form>
-        <p>Don't have an account? <a href="register.php">Register here</a></p>
-    </div>
-
-    <?php
-    if (isset($_POST['login'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        
-        require_once 'config.php';
-
-        $stmt = $pdo->prepare('SELECT * FROM users WHERE username = :username');
-        $stmt->execute(['username' => $username]);
-        $user = $stmt->fetch();
-
-        if ($user && password_verify($password, $user['password'])) {
-            $_SESSION['username'] = $username;
-            header('Location: game.php');
-            exit();
-        } else {
-            echo '<p class="error">Invalid username or password.</p>';
-        }
-    }
-    ?>
-</body>
-</html>
+$message = "";
